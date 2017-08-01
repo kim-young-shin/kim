cb87e36b674d68b1e763c085778c4330ffef6372
kys <kys@kyss-iMac.local>
2017-08-02 08:24:02 +0900

    okokok

---
+++ abc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  abc
+//
+//  Created by kys on 02/08/2017.
+//  Copyright © 2017 kys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ abc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  abc
+//
+//  Created by kys on 02/08/2017.
+//  Copyright © 2017 kys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
